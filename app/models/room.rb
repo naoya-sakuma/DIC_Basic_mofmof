d07bbfe51e4941626c9e9fe565c8513891b9f06a
naoyasakua <4869ns@gmail.com>
2020-11-20 15:40:32 +0900

    バリデーションを追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,8 @@ class Room < ApplicationRecord
   has_many :stations, inverse_of: :room
   accepts_nested_attributes_for :stations
   validates :name, presence: true
+  validates :rent, presence: true
+  validates :adress, presence: true
+  validates :age, presence: true
+
 end
